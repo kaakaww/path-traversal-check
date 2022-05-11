07936250edcb4bc33783ecb276a3ee8894c82ce6
Topher Lamey <clamey@gmail.com>
2022-05-11 09:35:57 -0600

    Add tests

--- src/main/kotlin/com/stackhawk/pathtraversalcheck/HelloController.kt
+++ src/main/kotlin/com/stackhawk/pathtraversalcheck/HelloController.kt
@@ -5,11 +5,21 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Paths
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class HelloController {
+
+    companion object {
+        const val BASE_PATH = "/private/tmp/user_content"
+        val VALID_PATHS = setOf("/hello")
+
+        val logger = LoggerFactory.getLogger(HelloController::class.java)
+    }
+
     @GetMapping("/")
     fun index(): String = "Greetings from Spring Boot!"
 
@@ -21,31 +31,25 @@ class HelloController {
     @GetMapping("/file-check")
     fun fileTraversalCheck(
         filePath: String
-    ): String = getContent(filePath)
-
-    companion object {
-        const val BASE_PATH = "/Users/topher"
-        val VALID_PATHS = setOf("/.vimrc")
-
-        val logger = LoggerFactory.getLogger(HelloController::class.java)
-    }
+    ): ResponseEntity<String> = getContent(filePath)
 
     @Throws(IOException::class)
-    fun getContent(path: String): String {
+    fun getContent(path: String): ResponseEntity<String> {
         val normalizedPath: String = Paths.get(path).normalize().toString()
         logger.info("Normalized Path $normalizedPath")
-
         return if (VALID_PATHS.contains(normalizedPath)) {
             val file = File(BASE_PATH, normalizedPath)
             logger.info("Canonical Path ${file.canonicalPath}")
 
             if (file.canonicalPath.startsWith(BASE_PATH)) {
-                String(Files.readAllBytes(file.toPath()))
+                ResponseEntity.ok(String(Files.readAllBytes(file.toPath())))
             } else {
-                "Access Error"
+                logger.info("Canonical path is not valid")
+                ResponseEntity.internalServerError().body("Access Error")
             }
         } else {
-            "Access Error"
+            logger.info("Normalized Path is not valid")
+            ResponseEntity.internalServerError().body("Access Error")
         }
     }
 }
--- src/test/kotlin/com/stackhawk/pathtraversalcheck/PathTraversalCheckApplicationTests.kt
+++ src/test/kotlin/com/stackhawk/pathtraversalcheck/PathTraversalCheckApplicationTests.kt
@@ -1,13 +1,49 @@
 package com.stackhawk.pathtraversalcheck
 
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.web.servlet.MockMvc
+import com.stackhawk.pathtraversalcheck.HelloController.Companion.BASE_PATH
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 
 @SpringBootTest
-class PathTraversalCheckApplicationTests {
+@AutoConfigureMockMvc
+class PathTraversalCheckApplicationTests(@Autowired val mockMvc: MockMvc) {
 
-	@Test
-	fun contextLoads() {
-	}
+    @BeforeEach
+    fun beforeEach() {
+        val basePath = Paths.get(BASE_PATH)
+        val backup = Paths.get("$basePath.bak")
+        val file = "$basePath/hello"
+        Files.deleteIfExists(Paths.get(file))
+        Files.deleteIfExists(basePath)
+        Files.deleteIfExists(backup)
 
+        Files.createDirectory(basePath)
+        File(file).writeText("Hello World")
+        Files.copy(basePath, backup)
+    }
+
+    @Test
+    fun `get file hello works`() {
+        getPath("/hello", HttpStatus.OK)
+    }
+
+    @Test
+    fun `get hello in backup doesn't work`() {
+        getPath("/../usr_content.bak/hello", HttpStatus.INTERNAL_SERVER_ERROR)
+    }
+
+    fun getPath(path: String, status: HttpStatus) {
+        mockMvc.perform(MockMvcRequestBuilders.get("/file-check?filePath=$path"))
+            .andExpect(MockMvcResultMatchers.status().`is`(status.value()))
+    }
 }
